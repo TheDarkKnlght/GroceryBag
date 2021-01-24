7d71c8df175d9a55be0c7877644c9ea698333d83
Salman A <salmana.ar@gmail.com>
2021-01-24 08:50:32 +0530

    Some updates!

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.grocery_list, name = 'home'),
+    path('add', views.add_entry, name = 'add_entry'),
+    path('del/<int:id>', views.remove, name="del"),
+    path('update/<int:id>', views.update, name="update"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
-def post_list(request) :
+def grocery_list(request) :
     items = Item.objects.all()
     return render(request, 'cart/index.html', {'items': items})
+
+def home(request) :
+    return render(request, 'main/home.html', {})
+
+def add_entry(request) :
+    if request.method == "POST" :
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else :
+        form = ItemForm()
+        return render(request, 'cart/add.html', {'form': form})
+
+def remove(request, id) : 
+    item = Item.objects.get(id=id) 
+    item.delete() 
+    return redirect('home') 
